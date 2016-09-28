9ebbd7e677cf3f894a68591b4d8b756f1addd41d
枫上雾棋 <billydong@foxmail.com>
2016-09-28 21:07:28 +0800

    Update

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  var img_h =new Array(
+    "img/1.jpg",
+    "img/2.jpg",
+    "img/3.jpg"
+  );
+  var num_h = 0;
+  // setInterval(function(){
+  //   if(num_h == img_h.length - 1){
+  //     num_h = 0;
+  //   }else{
+  //     num_h += 1;
+  //   }
+  //   $('#head').css('backgroundImage', 'url(' + img_h[num_h] + ')');
+  // }, 15000);
+});
