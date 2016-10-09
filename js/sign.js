a411797603aaa38d69cc6476a90fbabf86fa8c34
fengshangwuqi <billydong@foxmail.com>
2016-10-09 14:33:54 +0800

    Update

--- js/sign.js
+++ js/sign.js
@@ -1,1 +1,16 @@
 
+jQuery.fn.formChange=function(){
+  $(this).siblings("form").hide();
+  $(this).show();
+  $(this).find(".form-focus").focus();
+}
+
+$(".btn_extra").click(function(){
+  $('#signUp_1').formChange();
+});
+$("#signUp_1 .btn").click(function(){
+  $('#signUp_2').formChange();
+});
+$("#signUp_2 .btn").click(function(){
+  $('#signUp_3').formChange();
+});
